b9cef57123b358b7e25cdbcc60d437686f9b99fd
Chuangjie Xu <cj-xu@outlook.com>
2022-08-17 15:35:05 +0200

    More explanation to the test cases

--- src/test/java/regiontypeinference/Main.java
+++ src/test/java/regiontypeinference/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("======================================== Test Case 1 ========================================");
+        System.out.println("File location: testcases/paperexamples/RunningExample.java");
         System.out.println("The running example code is given in the body of the method foo:\n    x = y.f;\n    y = new C();\n    y.f = x;");
         System.out.println();
         // analyze the method foo()
@@ -47,6 +48,7 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println("======================================== Test Case 2 ========================================");
+        System.out.println("File location: testcases/paperexamples/Node.java");
         System.out.println("An example of linked lists given in Appendix B");
         System.out.println();
         // analyze the method last()
@@ -70,28 +72,36 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println("======================================== Test Case 3 ========================================");
+        System.out.println("File location: testcases/paperexamples/Parameter.java");
         System.out.println("A simple test case to check if parameter types are passed correctly.");
         System.out.println();
         // analyze the method f()
         runTestCase("testcases.paperexamples.Parameters", "f");
+        System.out.println("The object created at line 6 is passed via the methods id and id2 and in the end returned by f.");
         System.out.println("==================================== End of Test Case 3 =====================================");
 
         System.out.println();
         System.out.println();
         System.out.println("======================================== Test Case 4 ========================================");
+        System.out.println("File location: testcases/paperexamples/Vairable.java");
         System.out.println("A simple test case to check the strong update for variables.");
         System.out.println();
         // analyze the method h()
         runTestCase("testcases.paperexamples.Vairable", "h");
+        System.out.println("There are multiple assignments for the variable x.\n" +
+                "The return type indicates that in the end x points to the object created in line 8.");
         System.out.println("==================================== End of Test Case 4 =====================================");
 
         System.out.println();
         System.out.println();
         System.out.println("======================================== Test Case 5 ========================================");
+        System.out.println("File location: testcases/paperexamples/Field.java");
         System.out.println("A simple test case to check the weak update for fields.");
         System.out.println();
         // analyze the method h()
         runTestCase("testcases.paperexamples.Field", "h");
+        System.out.println("There are multiple assignments for the field <created at .(Field.java:18)>.f.\n" +
+                "Thus it has the type {<created at .(Field.java:19)>, <created at .(Field.java:21)>} due to the weak update.");
         System.out.println("==================================== End of Test Case 5 =====================================");
     }
 
