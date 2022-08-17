75582b41f021115506f13aba36240758595720a1
Chuangjie Xu <cj-xu@outlook.com>
2022-08-17 15:19:08 +0200

    make the example slightly more complicated

--- src/test/java/testcases/paperexamples/Parameters.java
+++ src/test/java/testcases/paperexamples/Parameters.java
@@ -4,12 +4,18 @@ public class Parameters {
 
     Object f() {
         Object arg = new Object();
-        Object obj = this.id(arg);
+        Object obj = id(arg);
+        obj = id2(obj);
         return obj;
     }
 
     Object id(Object par) {
         Object obj = par;
+        return obj;
+    }
+
+    Object id2(Object par) {
+        Object obj = id(par);
         return obj;
     }
 }
